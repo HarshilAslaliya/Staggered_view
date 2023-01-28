b4e24de782f9f35558d3cba51912501e30eae963
Harshil <harshilaslaliya@gmail.com>
2023-01-28 11:51:08 +0530

    first commit

---
+++ lib/modals/globals.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+List<Map<String, dynamic>> details = [
+  {
+    'image': AssetImage('assets/images/burj1.jpg'),
+    'height': 320.0,
+    'name': 'Burj Khalifa',
+    'details': "Burj Khalifa was designed by a team led by Adrian Smith of Skidmore,"
+        " a previous record holder for the world's tallest building. "
+        "Hyder Consulting was chosen to be the supervising engineer with NORR"
+        " Group Consultants International Limited chosen to supervise the architecture"
+        " of the project. The design is derived from the Islamic architecture of the region,"
+        " such as in the Great Mosque of Samarra.",
+    'photoslist': [
+      'assets/images/burj1.jpg',
+      'assets/images/burj3.jpg',
+      'assets/images/burj4.jpg',
+      'assets/images/burj5.jpg',
+    ],
+  },
+  {
+    'image': AssetImage('assets/images/indiagate.jpg'),
+    'height': 150.0,
+    'name': 'Indiagate',
+    'details': "The India Gate (formerly known as the All India War Memorial)"
+        " is a war memorial located near the Rajpath on the eastern edge of the "
+        "ceremonial axis of New Delhi,"
+        " formerly called duty path.",
+    'photoslist': [
+      'assets/images/indiagate.jpg',
+      'assets/images/indiagate2.jpg',
+      'assets/images/mumbai3.jpg',
+    ],
+  },
+  {
+    'image': AssetImage('assets/images/efil1.jpg'),
+    'height': 320.0,
+    'name': 'Eiffel Tower',
+    'details':
+        "Locally nicknamed La dame de fer (French for Iron Lady), it was constructed from 1887 to 1889 as the centerpiece of the 1889 World's Fair. Although initially criticised by some of France's leading artists and intellectuals for its design.",
+    'photoslist': [
+      'assets/images/efil1.jpg',
+      'assets/images/efil2.jpg',
+      'assets/images/efil3.jpg',
+      'assets/images/efil4.jpg',
+    ],
+  },
+  {
+    'image': AssetImage('assets/images/goa3.jpg'),
+    'height': 175.0,
+    'name': 'Goa',
+    'details':
+        "Panaji is the state's capital, while Vasco da Gama is its largest city. The historic city of Margão in Goa still exhibits the cultural influence of the Portuguese, who first voyaged to the subcontinent in the early 16th century as merchants.",
+    'photoslist': [
+      'assets/images/goa.jpg',
+      'assets/images/goa2.jpg',
+      'assets/images/goa3.jpg',
+      'assets/images/goa4.jpg',
+    ],
+  },
+  {
+    'image': AssetImage('assets/images/mountain1.jpg'),
+    'height': 320.0,
+    'name': 'Mountain',
+    'details':
+        "A mountain is an elevated portion of the Earth's crust, generally with steep sides that show significant exposed bedrock.",
+    'photoslist': [
+      'assets/images/mountain3.jpg',
+      'assets/images/mountain2.jpg',
+      'assets/images/mountain1.jpg',
+      'assets/images/mountain4.jpg',
+    ],
+  },
+  {
+    'image': AssetImage('assets/images/statue1.jpg'),
+    'height': 140.0,
+    'name': 'Statue Of Unity',
+    'details':
+        "The Statue of Unity is the world's tallest statue, with a height of 182 metres, located near Kevadia in the state of Gujarat, India.",
+    'photoslist': [
+      'assets/images/statue2.jpg',
+      'assets/images/statue3.jpg',
+      'assets/images/statue1.jpg',
+      'assets/images/statue4.jpg',
+    ],
+  },
+  {
+    'image': AssetImage('assets/images/taj1.jpg'),
+    'height': 160.0,
+    'name': 'Taj Mahal',
+    'details':
+        "An immense mausoleum of white marble, built in Agra between 1631 and 1648 by order of the Mughal emperor Shah Jahan in memory of his favourite wife, the Taj Mahal is the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage.",
+    'photoslist': [
+      'assets/images/taj2.jpg',
+      'assets/images/taj3.jpg',
+      'assets/images/taj1.jpg',
+      'assets/images/taj4.jpg',
+    ],
+  },
+  {
+    'image': AssetImage('assets/images/temple1.jpg'),
+    'height': 190.0,
+    'name': 'Temples',
+    'details':
+        "The form and function of temples are thus very variable, though they are often considered by believers to be, in some sense, the house of one or more deities. ",
+    'photoslist': [
+      'assets/images/temple3.jpg',
+      'assets/images/temple2.jpg',
+      'assets/images/temple1.jpg',
+      'assets/images/temple4.jpg',
+    ],
+  },
+];
